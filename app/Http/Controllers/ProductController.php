c4bda1fef51cca8061786d3c7a01d5a1d3126e41
orgin <lantung.2k@gmail.com>
2022-03-17 09:15:47 +0700

    update database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -27,7 +27,6 @@ class ProductController extends Controller
 
     public function store(Request $request)
     {
-//    $request->validate([]
         $this->productRepository->store($request);
         return redirect()->route('products.index');
     }
