85d06c0ad70509752107909cc088a94733c7d3d5
orgin <lantung.2k@gmail.com>
2022-03-17 08:47:13 +0700

    update database

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('backend.master');
@@ -20,7 +29,7 @@ Route::prefix('products')->group(function (){
 
 
 Route::prefix('category')->group(function () {
-    Route::get('category', [CategoryController::class, 'index'])->name('category.index');
+    Route::get('/index', [CategoryController::class, 'index'])->name('category.index');
     Route::get('{id}/detail', [CategoryController::class, 'show'])->name('category.detail');
     Route::get('{id}/delete', [CategoryController::class, 'destroy'])->name('category.destroy');
     Route::get('{id}/update', [CategoryController::class, 'edit'])->name('category.edit');
