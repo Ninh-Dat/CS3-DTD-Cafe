f357576e0e1967c3c1c3394546945835499d983b
orgin <lantung.2k@gmail.com>
2022-03-17 16:39:31 +0700

    update navbar

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,7 +10,7 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <img src="{{(asset('storage/'.z3261193396006_e0419c46d9d6279907a41e17c668802c.jpg)}}" alt=""  class="navbar-brand" >
+    <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
