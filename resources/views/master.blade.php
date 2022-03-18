eee3fab7913fa9e8d986a1770c16ce0d18925ae9
leminhtuan96 <tuanmokc@gmail.com>
2022-03-18 14:06:03 +0700

    update master

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,24 +10,32 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="products.create"><img width="35px" src="{{asset('storage/images/mat.jpg')}}" alt="logo"></a>
+    <a class="navbar-brand" href="products.create"><img width="50px" src="{{asset('storage/images/logo.png')}}" alt="logo"></a>
+    <div class="navbar-brand"
+         style="font-size: 40px ; font-family: 'Comic Sans MS',cursive,sans-serif; color:#704e29 ">
+        TDT Coffee
+    </div>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
+
+
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="{{route('category.index')}}">Thể loại<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="{{route('products.index')}}">Sản phẩm<span class="sr-only">(current)</span></a>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown">Quản lý đồ uống</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{route('category.index')}}">Loại đồ uống</a>
+                    <a class="dropdown-item" href="{{route('products.index')}}">Đồ uống</a>
+                </div>
+
+            <li class="nav-item">
+                <a class="nav-link" style="color: #2cc0af" href="{{route('products.create')}}">Thêm đồ uống</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" style="color: #2cc0af" href="{{route('products.create')}}">Thêm sản phẩm</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link"  style="color:blue" href="{{route('category.create')}}">Thêm thể loại</a>
+                <a class="nav-link"  style="color:blue" href="{{route('category.create')}}">Thêm loại đồ uống</a>
             </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
